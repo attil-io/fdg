a5fc71531af037ffbf35fd8ef9f81a69546d41da
Attilio <parrotchaffinch@gmail.com>
2018-04-01 11:49:16 +0200

    Render words into lines

--- src/js/fdg.js
+++ src/js/fdg.js
@@ -14,20 +14,40 @@ bodyElement.onload = function () {
 }
 
 function splitTextToWords(text) {
-	return text.replace(/\s\s+/g, ' ').match(/[a-zA-Z\u00E0-\u00FC\u00DF]+|\s+|[^\sa-zA-Z\u00E0-\u00FC\u00DF]+/g).filter(w => (w != ' ' && w != '\t' && w != '\n'));
+	return text.replace(/\s\s+/g, ' ').match(/[a-zA-Z\u00E0-\u00FC\u00DF]+|\s+|[^\sa-zA-Z\u00E0-\u00FC\u00DF]+/g).filter(w => (w != ' ' && w != '\t'));
 }
 
+var actualLineElement;
 function clearChoosableWords() {
 	while (chooseWordsElement.firstChild) {
 		    chooseWordsElement.removeChild(chooseWordsElement.firstChild);
 	}
+	actualLineElement = null;
+}
+
+function newLineForChoosableWords() {
+	actualLineElement = document.createElement("div");
+	chooseWordsElement.appendChild(actualLineElement);
+}
+
+function createChoosableWord(id, word) {
+	var wordElement = document.createElement("label");
+	wordElement.innerHTML = word;
+	wordElement.id = id;
+	actualLineElement.appendChild(wordElement);
 }
 
 function saveInputOnClick() {
+	clearChoosableWords();
+	newLineForChoosableWords();
 	var text = inputTextElement.value;
 	var words = splitTextToWords(text);
-	words.forEach(function(word) {
-		console.log(word);
+	words.forEach(function(word, index) {
+		if ("\n" === word) {
+			newLineForChoosableWords();
+		} else {
+			createChoosableWord(index, word);
+		}
 	});
 }
 })();
