90a8468ca0b8649a72a1a88956ced859a9330237
Attilio <parrotchaffinch@gmail.com>
2018-04-01 11:30:42 +0200

    Log out the words on click

--- src/js/fdg.js
+++ src/js/fdg.js
@@ -1,16 +1,34 @@
 (function () {
 
 var bodyElement = document.getElementsByTagName("body")[0]; 
+var inputTextElement, saveInputElement, chooseWordsElement;
+
 
 bodyElement.onload = function () {
 	console.log("Initializing fdg.js");
-	var inputTextElement = document.getElementById("inputtext");
-	var saveInputElement = document.getElementById("saveinput");
+	inputTextElement = document.getElementById("inputtext");
+	saveInputElement = document.getElementById("saveinput");
+	chooseWordsElement = document.getElementById("choosewords");
 
 	saveInputElement.onclick = saveInputOnClick;
 }
 
+function splitTextToWords(text) {
+	return text.replace(/\s\s+/g, ' ').match(/\w+|\s+|[^\s\w]+/g).filter(w => (w != ' ' && w != '\t' && w != '\n'));
+}
+
+function clearChoosableWords() {
+	while (chooseWordsElement.firstChild) {
+		    chooseWordsElement.removeChild(chooseWordsElement.firstChild);
+	}
+}
+
 function saveInputOnClick() {
-	console.log("clicked!");
+	var text = inputTextElement.value;
+	var words = splitTextToWords(text);
+	words.forEach(function(word) {
+		console.log(word);
+	});
 }
 })();
+
