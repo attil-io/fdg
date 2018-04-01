a027f3d21b984311bf2a743d880c7123d6c630d1
Attilio <parrotchaffinch@gmail.com>
2018-04-01 11:11:08 +0200

    Move click handler wiring to js

--- src/js/fdg.js
+++ src/js/fdg.js
@@ -1,2 +1,16 @@
-alert("Hello, world!");
+(function () {
 
+var bodyElement = document.getElementsByTagName("body")[0]; 
+
+bodyElement.onload = function () {
+	console.log("Initializing fdg.js");
+	var inputTextElement = document.getElementById("inputtext");
+	var saveInputElement = document.getElementById("saveinput");
+
+	saveInputElement.onclick = saveInputOnClick;
+}
+
+function saveInputOnClick() {
+	console.log("clicked!");
+}
+})();
